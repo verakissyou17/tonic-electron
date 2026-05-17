93d067475e15898bce3531d58ae94ef60fc57e40
verakissyou17 <130534541+verakissyou17@users.noreply.github.com>
2026-05-17 23:06:35 +0300

    Make the scroll button to the top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import PageNotFound from "./pages/PageNotFound";
 import Cart from "./pages/Cart";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
+      <ScrollToTop />
     </>
   );
 }
