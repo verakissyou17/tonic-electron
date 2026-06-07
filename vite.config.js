d5f76a671e919e70c9a6a546162b9b30186acad3
verakissyou17 <130534541+verakissyou17@users.noreply.github.com>
2026-06-07 18:28:27 +0300

    Configure vite.config file

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/tonic-electron/",
-  server: {
-    historyApiFallback: true,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === "build" ? "/repository-name/" : "/",
+    server: {
+      historyApiFallback: true,
+    },
+  };
 });
