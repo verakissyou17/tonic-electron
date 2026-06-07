ecf0c133aed11b1b1ee818b3e0183ba71fbf918e
verakissyou17 <130534541+verakissyou17@users.noreply.github.com>
2026-06-07 19:26:45 +0300

    Change images paths and use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import { ProductsProvider } from "./context/ProductsProvider.jsx";
 import { CartProvider } from "./context/CartProvider.jsx";
@@ -9,7 +9,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ProductsProvider>
         <CartProvider>
           <OrdersProvider>
@@ -17,6 +17,6 @@ createRoot(document.getElementById("root")).render(
           </OrdersProvider>
         </CartProvider>
       </ProductsProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
