583620dd2ec714e9efbf37eafabc44b7e536867f
verakissyou17 <130534541+verakissyou17@users.noreply.github.com>
2026-06-07 19:07:13 +0300

    fix: update repository name in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === "build" ? "/repository-name/" : "/",
+    base: command === "build" ? "/tonic-electron/" : "/",
     server: {
       historyApiFallback: true,
     },
